e36566bd1c67ec92654bfedf4fc41e3d848a2fad
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-07-11 01:45:39 -0600

    Modify date_added attribute of Topic
    - The date_added attribute now stores a complete dateTime, instead of
    only storing the date.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -5,7 +5,7 @@ class Topic(models.Model):
     """A topic the user is learning about."""
 
     text = models.CharField(max_length=200)
-    date_added = models.DateField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """Return a string representation of the model."""
