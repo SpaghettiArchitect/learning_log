96a8528024fc7cd919648eac658850ec9347b499
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-07-30 01:25:11 -0600

    Update views to display public topics
    - All public topics can now be displayed in the topics page, no matter
    if the user is authenticated or not.
    
    - The user can add and edit the entries of the public entries that they
    own, but not the public entries of other users.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -11,22 +11,41 @@ def index(request):
     return render(request, "learning_logs/index.html")
 
 
-@login_required
 def topics(request):
-    """Show all topics."""
-    topics = Topic.objects.filter(owner=request.user).order_by("date_added")
-    context = {"topics": topics}
+    """Show all of the current user's topics, and public topics that belong
+    to other users.
+    """
+    # If the user is logged in, get all their topics and the public topics
+    # from other users.
+    if request.user.is_authenticated:
+        # Get private topics.
+        topics = Topic.objects.filter(owner=request.user).order_by("date_added")
+        # Get public topics from all other users.
+        public_topics = Topic.objects.filter(public=True).order_by("date_added")
+    else:
+        # The user is not authenticated; return only public topics.
+        topics = None
+        public_topics = Topic.objects.filter(public=True).order_by("date_added")
+
+    context = {"topics": topics, "public_topics": public_topics}
     return render(request, "learning_logs/topics.html", context)
 
 
-@login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
-    check_topic_owner(topic, request.user)
+
+    # We only want to show the new_entry and edit_entry links if the
+    # currently logged user owns this topic.
+    is_owner = request.user == topic.owner
+
+    # Show an error page if the topic belongs to someone else and it's not
+    # a public topic.
+    if (not is_owner) and (not topic.public):
+        raise Http404
 
     entries = topic.entry_set.order_by("-date_added")
-    context = {"topic": topic, "entries": entries}
+    context = {"topic": topic, "entries": entries, "is_owner": is_owner}
     return render(request, "learning_logs/topic.html", context)
 
 
